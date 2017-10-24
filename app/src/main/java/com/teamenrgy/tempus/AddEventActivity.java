1c78e076386beee6bdbe6d47c94f5f7a3bf43846
rathi22 <30855764+rathi22@users.noreply.github.com>
2017-10-25 02:03:37 +0530

    Add files via upload
    Successfully adding event to database

--- app/src/main/java/com/teamenrgy/tempus/AddEventActivity.java
+++ app/src/main/java/com/teamenrgy/tempus/AddEventActivity.java
@@ -1,7 +1,9 @@
 package com.teamenrgy.tempus;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -12,7 +14,18 @@ import android.widget.EditText;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Calendar;
+
+import static com.teamenrgy.tempus.R.id.btnAddEvent;
+import static com.teamenrgy.tempus.R.id.topic_name;
+import static com.teamenrgy.tempus.R.id.topics_list;
 
 /**
  * Created by Rajat Rathi on 20-10-2017.
@@ -20,30 +33,38 @@ import java.util.Calendar;
 
 public class AddEventActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button btnDatePicker, btnStartTimePicker, btnEndTimePicker;
-    EditText txtDate, txtStartTime, txtEndTime;
+    Button btnDatePicker, btnStartTimePicker, btnEndTimePicker, btnAddEvent;
+    EditText txtDate, txtStartTime, txtEndTime, Category, Description;
     private int mYear, mMonth, mDay, mHour, mMinute;
+    String name, events, pending_events;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
         Toast.makeText(getBaseContext(), "Add Event!", Toast.LENGTH_SHORT).show();
+        Intent intent = getIntent();
+        events = intent.getStringExtra("events");
+        pending_events = intent.getStringExtra("pending_events");
+        name = intent.getStringExtra("name");
         btnDatePicker = (Button) findViewById(R.id.btn_date);
         btnStartTimePicker = (Button) findViewById(R.id.btn_stime);
         btnEndTimePicker = (Button) findViewById(R.id.btn_etime);
+        btnAddEvent = (Button) findViewById(R.id.btnAddEvent);
         txtDate = (EditText)findViewById(R.id.in_date);
         txtStartTime = (EditText) findViewById(R.id.in_stime);
         txtEndTime = (EditText) findViewById(R.id.in_etime);
+        Category = (EditText) findViewById(R.id.category);
+        Description = (EditText) findViewById(R.id.description);
         btnDatePicker.setOnClickListener(this);
         btnStartTimePicker.setOnClickListener(this);
         btnEndTimePicker.setOnClickListener(this);
+        btnAddEvent.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         if (v == btnDatePicker) {
-
             // Get Current Date
             final Calendar c = Calendar.getInstance();
             mYear = c.get(Calendar.YEAR);
@@ -63,7 +84,7 @@ public class AddEventActivity extends AppCompatActivity implements View.OnClickL
                             if(monthOfYear<9)
                                 mm = "0" + mm;
 
-                            txtDate.setText(year + "-" + mm + "-" + dd);
+                            txtDate.setText(year + "-" + mm + "-" + dd + " ");
                         }
                     }, mYear, mMonth, mDay);
             datePickerDialog.show();
@@ -113,6 +134,39 @@ public class AddEventActivity extends AppCompatActivity implements View.OnClickL
                         }
                     }, mHour, mMinute, false);
             timePickerDialog.show();
+        }
+        if (v == btnAddEvent) {
+            Response.Listener<String> addEventListener = new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+                        String id = jsonResponse.getString("id");
+                        int len = 4-id.length();
+                        for(int i=0;i<len;i++)
+                            id = "0"+id;
+                        events += id;
+                        Intent intent = new Intent(AddEventActivity.this, EventsActivity.class);
+                        intent.putExtra("events", events);
+                        intent.putExtra("pending_events", pending_events);
+                        AddEventActivity.this.startActivity(intent);
+                        Toast.makeText(getBaseContext(), "Success", Toast.LENGTH_SHORT).show();
+                    } catch (JSONException e) {
+                        Toast.makeText(getBaseContext(), "Fail", Toast.LENGTH_SHORT).show();
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            AddEventRequest addEventRequest = new AddEventRequest(
+                    Category.getText().toString(),
+                    Description.getText().toString(),
+                    name,
+                    txtDate.getText().toString() + txtStartTime.getText().toString(),
+                    txtDate.getText().toString() + txtEndTime.getText().toString(),
+                    addEventListener);
+            RequestQueue queue = Volley.newRequestQueue(AddEventActivity.this);
+            queue.add(addEventRequest);
         }
     }
 }
